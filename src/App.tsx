d6930cf83e8b4a9f3d0fe36c8177d01c0adc5a3c
Bruno Barretta <bruno.barretta@fatec.sp.gov.br>
2023-11-12 22:00:41 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import About from './components/About'
+import Contact from './components/Contact'
+import Experience from './components/Experience'
+import Navbar from './components/Navbar'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="text-gray-400 bg-gray-900 body-font">
+        <Navbar />
+        <About />
+        <Projects />
+        <Skills />
+        <Experience />
+        <Contact />
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/data/data.tsx
@@ -0,0 +1,37 @@
+
+const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://www.wildnatureimages.com/images/640/070620-014-The-Tetons.jpg",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://www.wildnatureimages.com/images/640/070620-014-The-Tetons.jpg",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://www.wildnatureimages.com/images/640/070620-014-The-Tetons.jpg",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://www.wildnatureimages.com/images/640/070620-014-The-Tetons.jpg",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+  export default projects
