0feedae6032815d86cfeab672bda320fad45df49
Bruno Barretta <bruno.barretta@fatec.sp.gov.br>
2023-11-13 23:13:07 -0300

    experience update

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,5 +1,6 @@
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import { FaStar, FaGraduationCap, FaSuitcase, FaGlobeAsia} from 'react-icons/fa';
 
 
 const Experience = () => {
@@ -9,7 +10,7 @@ const Experience = () => {
         <div className="flex flex-col w-full mb-20">
           {/* <CodeIcon className="mx-auto inline-block w-10 mb-4" /> */}
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-            Experience
+            Timeline
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
@@ -23,36 +24,38 @@ const Experience = () => {
           className="vertical-timeline-element--work"
           contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-          date="2011 - present"
+          date="09/2022 - present"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          // icon={<WorkIcon />}
+          icon={<FaSuitcase />}
         >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <h3 className="vertical-timeline-element-title">Front-end Developer</h3>
+          <h4 className="vertical-timeline-element-subtitle">Codex</h4>
+          <h4 className="vertical-timeline-element-subtitle">Campinas, SP</h4>
           <p>
             Creative Direction, User Experience, Visual Design, Project Management, Team Leading
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2010 - 2011"
+          date="02/2022 - 09/2022"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          // icon={<WorkIcon />}
+          icon={<FaSuitcase />}
         >
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+          <h3 className="vertical-timeline-element-title">Fullstack Developer</h3>
+          <h4 className="vertical-timeline-element-subtitle">Kpeyes</h4>
+          <h4 className="vertical-timeline-element-subtitle">Indaiatuba, SP</h4>
           <p>
             Creative Direction, User Experience, Visual Design, SEO, Online Marketing
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2008 - 2010"
+          date="2019 - 2021"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          // icon={<WorkIcon />}
+          icon={<FaGlobeAsia />}
         >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+          <h3 className="vertical-timeline-element-title">Career Break</h3>
+          <h4 className="vertical-timeline-element-subtitle">Nagoya, Japan</h4>
           <p>
             User Experience, Visual Design
           </p>
@@ -61,53 +64,30 @@ const Experience = () => {
           className="vertical-timeline-element--work"
           date="2006 - 2008"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          // icon={<WorkIcon />}
+          icon={<FaSuitcase />}
         >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+          <h3 className="vertical-timeline-element-title">Fullstack Developer</h3>
+          <h4 className="vertical-timeline-element-subtitle">Kpeyes</h4>
+          <h4 className="vertical-timeline-element-subtitle">Indaiatuba, SP</h4>
           <p>
             User Experience, Visual Design
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="April 2013"
+          date="2015 - 2017"
           iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-          // icon={<SchoolIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-          <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-          <p>
-            Strategy, Social Media
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="November 2012"
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-          // icon={<SchoolIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2002 - 2006"
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-          // icon={<SchoolIcon />}
+          icon={<FaGraduationCap />}
         >
           <h3 className="text-lg font-bold text-purple-600">System Analysis and Development</h3>
           <h4 className="vertical-timeline-element-subtitle">Bachelor of Technology</h4>
-          <p>
+          {/* <p>
             Creative Direction, Visual Design
-          </p>
+          </p> */}
         </VerticalTimelineElement>
         <VerticalTimelineElement
           iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-          // icon={<StarIcon />}
+          icon={<FaStar />}
         />
       </VerticalTimeline>
 
